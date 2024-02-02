4f13abf06d0482892313f310c22cb1d9462364da
韩杰 <hanjie@blocgo.com>
2024-02-02 10:34:53 +0800

    init

--- errors.go
+++ errors.go
@@ -4,10 +4,9 @@ import (
 	"errors"
 	"fmt"
 
+	httpstatus "github.com/go-kratos/kratos/v2/transport/http/status"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/status"
-
-	httpstatus "github.com/go-kratos/kratos/v2/transport/http/status"
 )
 
 const (
